37a8efd292b27a60339c8b36d185153d08bff5b1
Gabriel P <mail.gabriel.pollak@gmail.com>
2025-07-10 16:03:24 +0000

    feat: initial Hello World app

---
+++ HelloWorldApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HelloWorldApp
+{
+    public static class Program
+    {
+        public static string GetMessage() => "Hello World";
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(GetMessage());
+        }
+    }
+}
